92dce1b9c3c67001185da578c9c4bf6650bf769b
Nour Souid <noursouidd@gmail.com>
2020-04-27 15:33:06 -0400

    removed coma

--- db/migrate/20200427184807_devise_create_users.rb
+++ db/migrate/20200427184807_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :first_name,
-      t.string :last_name,
-      t.string :address,
-      t.string :city,
-      t.string :province,
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :province
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
