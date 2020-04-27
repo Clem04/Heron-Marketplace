134a2e1b657e5adb7f8c5da6f9323a00b6a3959d
Nour Souid <noursouidd@gmail.com>
2020-04-27 15:40:38 -0400

    allows min length to 2 for the user's first name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, length: { minimum: 2 }
   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
   devise :database_authenticatable, :registerable,
