096f96baed6cbba60a03fe6b32d6bc26d904ce89
Nour Souid <noursouidd@gmail.com>
2020-04-30 16:10:41 -0400

    skipped user authentication in the home page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   end
 
   get '/merchants/:id', to: 'merchants#show', as: 'merchant'
-  # merchant GET    /categories/:category_id/products(.:format)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
