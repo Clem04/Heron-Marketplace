7e081833f099885b22f6bcf64b7e75c39907dafb
Nour Souid <noursouidd@gmail.com>
2020-04-30 16:08:48 -0400

    route to merchant show

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :categories, only: [] do
     resources :products, only: [:index, :show]
   end
+
+  get '/merchants/:id', to: 'merchants#show', as: 'merchant'
+  # merchant GET    /categories/:category_id/products(.:format)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
