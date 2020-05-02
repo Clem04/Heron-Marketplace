2c1b656d40f56d701580cf54caf8dc3773f9cc37
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-05-02 17:21:50 -0400

    Added Associastions to label, post and post-label models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :post_labels
+  has_many :labels, through: :post_labels
 end
--- app/models/post_label.rb
+++ app/models/post_label.rb
@@ -1,4 +1,2 @@
 class PostLabel < ApplicationRecord
-  belongs_to :post
-  belongs_to :label
 end
