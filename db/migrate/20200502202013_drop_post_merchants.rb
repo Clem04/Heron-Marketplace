583d38af1b5976b35c01085adb931736b31e0fce
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-05-02 16:36:50 -0400

    Droped post Merchants table

--- db/migrate/20200502202013_drop_post_merchants.rb
+++ db/migrate/20200502202013_drop_post_merchants.rb
@@ -1,4 +1,8 @@
 class DropPostMerchants < ActiveRecord::Migration[5.2]
-  def change
+  def up
+    drop_table :post_merchants
+  end
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
