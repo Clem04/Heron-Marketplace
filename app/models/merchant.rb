788aa6c1fbf72bc3fc3ea8c6b164a4fcfa84931b
Rose Grondin <rose.grondin@gmail.com>
2020-05-04 11:40:41 -0400

    design merchant product list

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,4 @@
 class Merchant < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :category, through: :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :sub_category
   belongs_to :merchant
+  has_one :category, through: :sub_category
 end
