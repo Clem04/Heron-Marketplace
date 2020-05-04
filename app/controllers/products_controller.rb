bf901d85e449c653c93bcca5c68cacbbbe408872
Nour Souid <noursouidd@gmail.com>
2020-05-04 17:50:24 -0400

    added associations products to category model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
-    @products = @category.sub_categories.map do |sub_category|
-      sub_category.products
+    @products = @category.products.map do |product|
+      product
     end
   end
 
