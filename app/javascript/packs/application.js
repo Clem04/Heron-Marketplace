2b300ce91dc82801ccefaf1e10d27a5077f1ac5d
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-05-08 14:36:32 -0400

    Resolve conflicts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,24 +1,20 @@
 import "bootstrap";
 import { menu_toggle } from "../plugin/form-nav";
-<<<<<<< HEAD
-import { menu_toggle } from "../plugin/confirmation-merchant";
-=======
+import { confirmation_message } from "../plugin/confirmation-merchant";
 import { openNav } from "../plugin/open-menu";
 import { closeNav } from "../plugin/close-menu";
 
 openNav();
 closeNav();
->>>>>>> db94d3c568d2b8b78004bd12edcce28b142fdf29
 
 if(window.location.pathname === '/users/sign_up'){
   menu_toggle();
 };
-<<<<<<< HEAD
 
 if(window.location.pathname === '/posts/new'){
-  confirmation_message();
-=======
+  confirmation_message()
+};
+
 if(window.location.pathname === '/users/sign_in'){
   menu_toggle();
->>>>>>> db94d3c568d2b8b78004bd12edcce28b142fdf29
 };
