764416165f62e99558d09fca185b995cec6db60c
Nour Souid <noursouidd@gmail.com>
2020-05-19 14:16:19 -0400

    new view added form to add product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,6 +29,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :category_id, :price, photos: [])
+    params.require(:product).permit(:name, :description, :sub_category_id, :price, :photo_1)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   has_many :sub_categories, dependent: :destroy
   has_many :products, through: :sub_categories
   has_many :posts
+
+  validates :name, presence: true, inclusion: { in: ["Fashion", "Beauty & Wellness", "Food", "Home Decor"] }
 end
