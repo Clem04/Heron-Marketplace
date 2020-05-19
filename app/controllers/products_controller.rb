c8832be260e0dd339e8ec648dca2a7481eb8bc1c
Nour Souid <noursouidd@gmail.com>
2020-05-19 13:31:21 -0400

    action new and create for the product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,25 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @category = Category.find(params[:category_id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.merchant = current_user
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :price, photos: [])
+  end
 end
