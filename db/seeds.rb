13e4a3d62757b7d32db9a3c0505e788e823d73e4
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-05-19 09:00:19 -0400

    Removed accent to Goer in seeding - bug fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ nour = User.create!(first_name: 'Nour', last_name: 'Souid',
 ines = User.create!(first_name: 'ines', last_name: 'Belghiti',
   email: 'ines@heronmarket.ca', password: '123456',
   address: '5333 Casgrain Ave #102, Montreal, Quebec H2T 1X3', points: '300')
-katell = User.create!(first_name: 'Katell', last_name: 'Goaër',
+katell = User.create!(first_name: 'Katell', last_name: 'Goaer',
   email: 'katell@heronmarket.ca', password: '123456',
   address: '5333 Casgrain Ave #102, Montreal, Quebec H2T 1X3', points: '300')
 
