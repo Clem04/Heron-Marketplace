4f627353df4c2451448bb495810a06627eafaf8c
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-05-20 17:05:14 -0400

    Added Merchant Invitation configuration.. invitation deadline is two weeks

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,7 @@ class Merchant < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :products
   has_many :labels, -> { distinct }, through: :products
   has_many :category, through: :products
