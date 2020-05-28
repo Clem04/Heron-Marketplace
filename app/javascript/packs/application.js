4aa43e9b02ac30ecc64b4c46b8dbe77f3572dbdf
Katell Goaer <alt.katellgoaer@gmail.com>
2020-05-28 17:35:12 -0400

    added popup successfully added to your cart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,12 @@ import { confirmation_message } from "../plugin/confirmation-merchant";
 import { openNav } from "../plugin/open-menu";
 import { closeNav } from "../plugin/close-menu";
 import { openUserNav } from "../plugin/user-menu";
+import { addToCart } from "../plugin/add-to-cart"
 
 openNav();
 closeNav();
 openUserNav();
+addToCart();
 
 if(window.location.pathname === '/users/sign_up'){
   menu_toggle();
