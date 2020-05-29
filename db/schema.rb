ee0cb0b5b0a06c916d99dcc3a9a44093abb37866
Nour Souid <noursouidd@gmail.com>
2020-05-29 19:56:47 -0400

    added model product variant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_191847) do
+ActiveRecord::Schema.define(version: 2020_05_29_235353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,6 +173,13 @@ ActiveRecord::Schema.define(version: 2020_05_26_191847) do
     t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
+  create_table "product_variants", force: :cascade do |t|
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_variants_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -224,6 +231,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_191847) do
   add_foreign_key "post_labels", "labels"
   add_foreign_key "post_labels", "posts"
   add_foreign_key "posts", "categories"
+  add_foreign_key "product_variants", "products"
   add_foreign_key "products", "merchants"
   add_foreign_key "products", "sub_categories"
   add_foreign_key "sub_categories", "categories"
