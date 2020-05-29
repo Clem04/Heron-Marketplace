8a454802a4adc0a3a74f2f333e72f79d516b90b5
Nour Souid <noursouidd@gmail.com>
2020-05-29 18:00:29 -0400

    fixing bug

--- db/migrate/20200529213429_create_product_variants.rb
+++
@@ -1,9 +0,0 @@
-class CreateProductVariants < ActiveRecord::Migration[5.2]
-  def change
-    create_table :product_variants do |t|
-      t.references :products
-
-      t.timestamps
-    end
-  end
-end
