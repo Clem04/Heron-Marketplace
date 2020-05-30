82187a2041959dd1d51653cf21ace079de070bc9
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-05-29 20:50:07 -0400

    Updated invation form to allow the admin to enter comments on the brand prior on the merchant creating their account

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,7 @@ class Merchant < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :products
   has_many :labels, -> { distinct }, through: :products
   has_many :category, through: :products
