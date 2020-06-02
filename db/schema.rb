ed15e1ab3106d638767601ae8a660ec57b9fc5cb
Nour Souid <noursouidd@gmail.com>
2020-06-02 19:06:09 -0400

    added sku to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_153457) do
+ActiveRecord::Schema.define(version: 2020_06_02_225726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_153457) do
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
     t.bigint "sub_category_id"
+    t.string "sku"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
     t.index ["sub_category_id"], name: "index_products_on_sub_category_id"
   end
