c252d2431235075f991db57cec19dcbe68cea06b
Nour Souid <noursouidd@gmail.com>
2020-06-11 23:19:52 -0400

    style picture preview

--- app/javascript/plugin/photo-preview.js
+++ app/javascript/plugin/photo-preview.js
@@ -13,12 +13,24 @@ const displayPreview = (input) => {
   if (input.files && input.files[0]) {
     const reader = new FileReader();
     let preview = '';
-    if (input.id === 'product_photo_1') { preview = '.photo-preview-1' }
-    if (input.id === 'product_photo_2') {  preview = '.photo-preview-2' }
+    if (input.id === 'product_photo_1') {
+      preview = '.photo-preview-1'
+      const photo1 = document.querySelector('.photo-1');
+      photo1.style.position = 'relative';
+      photo1.style.top = '108px';
+      photo1.style.right = '150px';
+    }
+    if (input.id === 'product_photo_2') {
+      preview = '.photo-preview-2'
+      const photo2 = document.querySelector('.photo-2');
+      photo2.style.position = 'relative';
+      photo2.style.top = '108px';
+      photo2.style.right = '140px';
+    }
     reader.onload = (event) => {
       document.querySelector(preview).src = event.currentTarget.result;
     }
-    reader.readAsDataURL(input.files[0])
+    reader.readAsDataURL(input.files[0]);
     document.querySelector(preview).classList.remove('hidden');
   }
 }
