2f34ddc5352bbd51fb3833337c890940c1e145c6
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-15 13:05:55 -0400

    fixed error of null when no add to cart button

--- app/javascript/plugin/add-to-cart-icon-update.js
+++ app/javascript/plugin/add-to-cart-icon-update.js
@@ -4,11 +4,13 @@ const addToCartIconUpdate = () => {
   const navItemCount = navCartIcon.querySelector('.num-item-in-cart');
   const addToCartButton = document.querySelector('.add-cart-btn');
 
-  addToCartButton.addEventListener('click', (event) => {
-    navItemCount.innerHTML = parseInt(navItemCount.innerHTML) + 1;
-    navItemCount.classList.remove('no-display');
-    navBagImg.src = '/assets/shopping-bag-icon-black-b2246b7f388cfc73817fb4b0ade45778895e7c08e0da450718366e991103c3de.svg';
-  })
+  if (addToCartButton) {
+    addToCartButton.addEventListener('click', (event) => {
+      navItemCount.innerHTML = parseInt(navItemCount.innerHTML) + 1;
+      navItemCount.classList.remove('no-display');
+      navBagImg.src = '/assets/shopping-bag-icon-black-b2246b7f388cfc73817fb4b0ade45778895e7c08e0da450718366e991103c3de.svg';
+    })
+  }
 };
 
 export { addToCartIconUpdate };
