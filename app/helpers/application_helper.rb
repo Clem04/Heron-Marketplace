660e55777ee58c9853c67a0e655854e4e2173def
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-15 12:08:58 -0400

    changed number of items in cart in navbar to total number and not only number of different items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def cart_count_over_one
-    return total_cart_items if total_cart_items > 0
-  end
+  # def cart_count_over_one
+  #   return total_cart_items if total_cart_items > 0
+  # end
 
   def total_cart_items
     total = @cart.line_items.map(&:quantity).sum
