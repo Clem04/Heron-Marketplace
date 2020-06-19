aeec54cb7f9577655ba8f5b92aacf3114ab73531
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-06-19 15:24:08 -0400

    Created index, new and create methods for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,25 @@
 class OrdersController < ApplicationController
+	def index
+		@order = Order.all
+	end
+
+	def new
+		@order = Order.new
+	end
+
+	def create
+		@order = Order.new(order_params)
+		if @order.save
+			redirect_to order_path(@order)
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def oder_params
+		params.require(:order).permit(:user_id, :points_used, :points_earned, 
+			:price_before_reduction, :final_price_before_taxes, :final_price_with_taxes, :status )
+	end
 end
