7bc2feda71776978312c68934bbe1ecfd70dafcb
Nour Souid <noursouidd@gmail.com>
2020-06-21 15:36:00 -0400

    only one sub category appeares

--- app/javascript/plugin/form-input.js
+++ app/javascript/plugin/form-input.js
@@ -4,24 +4,24 @@ function handleClick(e) {
   const label = this.querySelector('label');
   const sub = document.querySelectorAll('.sub-categories-form');
   if (sub) {
-    sub.forEach(subCat => subCat.innerHTML = '');
+    sub.forEach(subCat => subCat.classList.add('hidden'));
   }
   if (label.innerText === 'Fashion') {
-    const subCatFash = sub.querySelector('.fashion-sub')
+    const subCatFash = form.querySelector('.fashion-sub')
     subCatFash.classList.remove('hidden')
   }
   if (label.innerText === 'Beauty & Wellness') {
-    const subCatFash = sub.querySelector('.beauty-sub')
+    const subCatFash = form.querySelector('.beauty-sub')
     subCatFash.classList.remove('hidden')
   }
 
   if (label.innerText === 'Food') {
-    const subCatFash = sub.querySelector('.food-sub')
+    const subCatFash = form.querySelector('.food-sub')
     subCatFash.classList.remove('hidden')
   };
 
   if (label.innerText === 'Home Decor') {
-    const subCatFash = sub.querySelector('.home-sub')
+    const subCatFash = form.querySelector('.home-sub')
     subCatFash.classList.remove('hidden')
   };
 }
