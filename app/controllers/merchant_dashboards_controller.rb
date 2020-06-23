20ad3ccf87ecdf03c3a737489cd5602de36c5734
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-06-22 22:56:22 -0400

    Added merchant id params to dashbaord controller

--- app/controllers/merchant_dashboards_controller.rb
+++ app/controllers/merchant_dashboards_controller.rb
@@ -3,9 +3,4 @@ class MerchantDashboardsController < ApplicationController
   	@dashboard = Dashboard.find(params[:id])
   	@merchant = Merchant.find(params[:merchant_id])
   end
-
-  private
-  def method_name
-  	
-  end
 end
