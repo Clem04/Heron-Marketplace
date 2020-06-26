63f544968e462921cf9d45c1cfbe7a4ebe0205c8
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-06-26 15:59:07 -0400

    Merchant onboard with stripe connect express

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -7,5 +7,17 @@ class StripeController < ApplicationController
 	        grant_type: "authorization_code"
 	      }
 	    )
+
+	    if response.parsed_response.key?("error")
+	      redirect_to user_settings_path(user),
+	        notice: response.parsed_response["error_description"]
+	    else
+	      stripe_user_id = response.parsed_response["stripe_user_id"]
+	      @merchant.update_attribute(:stripe_user_id, stripe_user_id)
+
+	      redirect_to user_settings_path(user),
+	        notice: 'User successfully connected with Stripe!'
+	    end
+
 	end
 end
