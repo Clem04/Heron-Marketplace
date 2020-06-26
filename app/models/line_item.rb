6fef12f04cb6460b7a1cf2f74ae5fe5614d1d02f
Clem <30819439+Clem04@users.noreply.github.com>
2020-06-26 12:39:53 -0400

    Updated price_cents to price based on money_rails gem

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,6 @@ class LineItem < ApplicationRecord
   belongs_to :cart
 
   def total_price
-    product_variant.product.price_cents.to_i * quantity.to_i
+    product_variant.product.price.to_i * quantity.to_i
   end
 end
