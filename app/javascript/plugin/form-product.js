f0f9f0e37de8554932791ce728d7fd555d7a964e
Nour Souid <noursouidd@gmail.com>
2020-06-29 10:28:11 -0400

    product sizes block appear when we select Diffrent Sizes

--- app/javascript/plugin/form-product.js
+++ app/javascript/plugin/form-product.js
@@ -1,16 +1,19 @@
 const input = document.querySelector('.size-input');
 const selectField = input.querySelector('select');
-const options = selectField.querySelectorAll('option');
+const sizesBlock = document.querySelector('.diffrent-sizes');
 
 function displayBlockDifSizes(e) {
-  console.log(e);
+  console.log(e.currentTarget.value);
+  if (e.currentTarget.value === 'Diffrent Sizes' ) {
+    sizesBlock.classList.remove('hidden');
+  } else {
+    sizesBlock.classList.add('hidden');
+  }
 }
-// if (selectField) {
-// }
-options.forEach((op) => {
-  // console.log(op);
-  op.addEventListener('change', displayBlockDifSizes);
-})
+if (selectField) {
+  selectField.addEventListener('change', displayBlockDifSizes)
+}
+
 
 
 export { displayBlockDifSizes };
