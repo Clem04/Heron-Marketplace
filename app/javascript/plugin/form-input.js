9f811b2b9b8c5c2c44566dd16af0696e34690a95
Nour Souid <noursouidd@gmail.com>
2020-07-03 12:41:30 -0400

    render subcategory depending on the category

--- app/javascript/plugin/form-input.js
+++ app/javascript/plugin/form-input.js
@@ -1,36 +1,36 @@
 const form = document.querySelector('.category-form-group');
 const inputSizes = document.querySelector('.size-input');
 const sub = document.querySelector('.sub-categories-form');
+const subCatSelect = sub.querySelector('select');
 const subCatOptions = sub.querySelectorAll('option');
-console.log(subCatOptions);
+let subCatOp = sub.querySelectorAll('option');
+
 
 function handleClick(e) {
   const label = this.querySelector('label');
-  if (sub) {
-    sub.classList.add('hidden');
-  }
-  if (label.innerText === 'Fashion') {
-    const subCatFash = form.querySelector('.fashion-sub')
-    subCatFash.classList.remove('hidden')
-    inputSizes.classList.remove('hidden')
-  } else {
-    inputSizes.classList.add('hidden')
-  }
-  if (label.innerText === 'Beauty & Wellness') {
-    const subCatFash = form.querySelector('.beauty-sub')
-    subCatFash.classList.remove('hidden')
-  }
+  sub.classList.remove('hidden');
+  const subCat = subCatOptions.forEach((option) => {
+    option.classList.add('hidden');
+    if (label.innerText === 'Fashion' && option.value <= 8) {
+      inputSizes.classList.remove('hidden');
+      option.classList.remove('hidden');
+    } else {
+      inputSizes.classList.add('hidden')
+    }
+    if (label.innerText === 'Beauty & Wellness' && option.value > 8 && option.value <= 16) {
+      option.classList.remove('hidden');
+    }
+    if (label.innerText === 'Food' && option.value > 16 && option.value <= 25) {
+      option.classList.remove('hidden');
+    };
 
-  if (label.innerText === 'Food') {
-    const subCatFash = form.querySelector('.food-sub')
-    subCatFash.classList.remove('hidden')
-  };
-
-  if (label.innerText === 'Home Decor') {
-    const subCatFash = form.querySelector('.home-sub')
-    subCatFash.classList.remove('hidden')
-  };
+    if (label.innerText === 'Home Decor' && option.value > 25 && option.value <= 37) {
+      option.classList.remove('hidden');
+    };
+  });
 }
+
+
 
 const formInput = () => {
   if (form) {
